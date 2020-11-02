406e09b26f0d08a38f9271bdb775cafde6a7ef75
Christopher Hertel <mail@christopher-hertel.de>
2020-11-02 23:15:33 +0100

    add background extension to code slide

--- src/Slide/CodeSlide.php
+++ src/Slide/CodeSlide.php
@@ -13,12 +13,14 @@ class CodeSlide extends Slide
     private string $filePath;
     private int $offset;
     private ?int $lines;
+    private int $minLineLength;
 
-    public function __construct(string $filePath, int $offset = 0, int $lines = null)
+    public function __construct(string $filePath, int $offset = 0, int $lines = null, int $minLineLenght = 60)
     {
         $this->filePath = $filePath;
         $this->offset = $offset;
         $this->lines = $lines;
+        $this->minLineLength = $minLineLenght;
     }
 
     public function render(): void
@@ -26,15 +28,15 @@ class CodeSlide extends Slide
         $code = file_get_contents($this->filePath);
         $lines = explode(PHP_EOL, $code);
         $height = $this->lines ? $this->lines : count($lines);
-        $width = max(array_map('mb_strlen', $lines)) + 5;
+        $width = max(max(array_map('mb_strlen', $lines)) + 5, $this->minLineLength);
         $terminal = new Terminal();
-        $paddingX = (int)floor(($terminal->getWidth() - $width) / 2);
+        $paddingX = (int)floor(($terminal->getWidth() - $width) / 2) - 2;
         $paddingY = (int)floor(($terminal->getHeight() - $height) / 2);
 
         $this->getOutput()->write(str_repeat(PHP_EOL, $paddingY));
 
         $source = SourceCodeHelper::create($this->getOutput())
-            ->highlightFile($this->filePath, $this->offset, $this->lines);
+            ->highlightFile($this->filePath, $this->offset, $this->lines, $width);
 
         foreach (explode(PHP_EOL, $source) as $line) {
             $padding = str_repeat(' ', $paddingX);
