a1c06f2badd8f4e711fccce460078940fc783859
Christopher Hertel <mail@christopher-hertel.de>
2020-11-02 23:14:53 +0100

    introduce extension point for CanvasSlide

--- src/Slide/CanvasSlide.php
+++ src/Slide/CanvasSlide.php
@@ -13,18 +13,29 @@ use Stoffel\Console\Canvas\Element\Rectangle;
 use Stoffel\Console\Canvas\Fill;
 use Stoffel\Console\Canvas\Position;
 
-class CanvasSlide extends Slide
+abstract class CanvasSlide extends Slide
 {
     public function render(): void
     {
         $canvas = CanvasHelper::create($this->getOutput());
-        $canvas
-            ->setBackground(Fill::withGradient(['#000000', '#999999']))
-            ->addElement(new Position(10, 10), new Rectangle(new Dimension(10, 5), Fill::withColor('#FF0000')))
-            ->addElement(new Position(50, 5), new Rectangle(new Dimension(20, 5), Fill::withGradient('summer')))
-            ->addElement(new Position(100, 5), new Rectangle(new Dimension(30, 25), Fill::withColor('#00FF00')))
-            ->addElement(new Position(10, 40), new Headline('Hello World', Fill::withColor('yellow')))
-            ->addElement(new Position(25, 13), new Image(dirname(__DIR__, 2).'/examples/php-logo.png', new Dimension(60, 30)))
-            ->draw();
+        $canvas->setBackground($this->getBackground());
+
+        foreach ($this->getElements() as $touple) {
+            $canvas->addElement(...$touple);
+        }
+
+        $canvas->draw();
+    }
+
+    /**
+     * Returns array of Position+Element touples.
+     *
+     * @return array
+     */
+    abstract protected function getElements(): array;
+
+    protected function getBackground(): ?Fill
+    {
+        return null;
     }
 }
